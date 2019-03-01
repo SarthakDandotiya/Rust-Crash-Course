f91c525a724ab7b659a14a73975742e361f76440
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-03-02 01:59:57 +0530

    Minor Edits

--- first/src/main.rs
+++ first/src/main.rs
@@ -3,13 +3,13 @@ fn main() {
 
     // IMPPORTANT: Every Statement in Rust should terminate with a 'Semicolon (;)'
 
-    // Rust puts a lot of emphasis in immutability. Immutability while keeping Concurrency in mind.
+    // Rust puts a lot of emphasis on immutability. Immutability while keeping Concurrency in mind.
 
-    // We use the 'let' Key-word to declare variables in Rust which is very similar to what Javascript does now, making things pretty easy
+    // We use the 'let' keyword to declare variables in Rust which is very similar to what Javascript does now, making things pretty easy.
 
     // Rust mainly has 4 main "Scalar" Types:
     // Integer, Floating Point, Boolean and Characters
-    // Then there are different types of each, like Integer could be signed & 8-bit ie.Specific to save space and in Rust Memory-Management is also a key factor 
+    // Then there are different types of each, like Integer could be signed & 8-bit ie.Specific to save space and in Rust Memory-Management is also a key factor.
 
     
     let x = 1; // Integer
@@ -20,9 +20,9 @@ fn main() {
     let dynamic_math = 8 * 8;
     let dynamic_math_2 = 8 - 2 + 8 * 2; // General Preceedence of operators is followed as other programming languages
 
-    // It is recommended or wished that you use 'Snake Case' in Rust and you will be prompted every now and then to do so too
+    // It is recommended or wished that you use 'Snake Case' in Rust and you will be prompted every now and then to do so.
 
-    // Rust will also complains if you try to run or compile a program with un-used variables
+    // Rust will also complain if you try to run or compile a program with un-used variables.
 
     // Some other data-types
     let my_array = [1, 2, 3, 4, 5, 6, 7]; // Array, similar to other programming languages
@@ -31,7 +31,7 @@ fn main() {
 
     let (dynamic_x, dynamic_y, dynamic_z) = my_tuple; // Using a tuple to create variables on the go.
 
-    // These are the basic Types. There are more, its suggested to go through them once but the ones mentioned here were the simplest ones
+    // These are the basic Types. There are more, its suggested to go through them once but the ones mentioned here were the simplest ones to know.
     
     println!("x = {}", x);
     println!("y = {}", y);
